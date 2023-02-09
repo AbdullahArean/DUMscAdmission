da32938740de2a1066ba99c67aee20819138aead
Alve Reduan <alve.reduan.09@gmail.com>
2023-02-10 04:43:56 +0600

    UPDATE SQLdump

--- database/sqldump.sql
+++ database/sqldump.sql
@@ -1,118 +1,134 @@
+DROP TABLE Users;
+DROP TABLE role;
+DROP TABLE Role;
+DROP TABLE roles;
+DROP TABLE Roles;
+DROP TABLE Applications;
 
-CREATE TABLE notice (
-  notice_id INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL,
-  notice_body varchar2(1500),
-  notice_file_path varchar2(100),
-  notice_date_time timestamp(0)
+
+alter session set container=orclpdb;
+ALTER DATABASE OPEN;
+show con_name;
+
+
+CREATE USER dumscdb IDENTIFIED BY "password";
+GRANT ALL PRIVILEGES TO dumscdb;
+
+SELECT * FROM dba_users WHERE username='DUMSCDB';
+SELECT * FROM dba_users;
+
+/* TABLES */
+
+CREATE TABLE Roles (
+    role_id NUMBER PRIMARY KEY,
+    role_name varchar(50) NOT NULL
+);
+
+CREATE TABLE Permissions (
+    permission_id NUMBER PRIMARY KEY,
+    permission_name varchar(50) NOT NULL
+);
+
+CREATE TABLE Role_Permissions (
+    role_permission_id NUMBER PRIMARY KEY,
+    role_id NUMBER REFERENCES Roles(role_id),
+    permission_id NUMBER REFERENCES Permissions(permission_id)
 );
 
 
-CREATE TABLE users (
-  u_id INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL,
-  u_mail varchar2(50) NOT NULL,
-  u_phone varchar2(20),
-  username varchar2(15) NOT NULL,
-  password varchar2(100) NOT NULL,
-  verified NUMBER(1,0) DEFAULT 0,
-  role_id number(10) NOT NULL,
-  u_picpath varchar2(100)
+CREATE TABLE Universities (
+    uni_id NUMBER(10) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY NOT NULL,
+    uni_name VARCHAR2(200) NOT NULL,
+    uni_type VARCHAR2(100),
+    uni_website VARCHAR2(200)
 );
 
-CREATE TABLE application (
-  app_id varchar2(15) PRIMARY KEY NOT NULL,
-  u_id varchar2(15) NOT NULL,
-  name varchar2(100) NOT NULL,
-  f_name varchar2(100) NOT NULL,
-  m_name varchar2(100) NOT NULL,
-  dob date NOT NULL,
-  a_picpath varchar2(100) NOT NULL,
-  a_sigpath varchar2(100) NOT NULL,
-  a_phone varchar2(15) NOT NULL,
-  a_mail varchar2(50) NOT NULL
+CREATE TABLE Subjects (
+    sub_id NUMBER(10) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY NOT NULL,
+    sub_name VARCHAR2(200) NOT NULL
 );
 
-
-CREATE TABLE ssc (
-  app_id varchar2(15) PRIMARY KEY NOT NULL,
-  ssc_roll varchar2(15) NOT NULL,
-  ssc_reg varchar2(15) NOT NULL,
-  ssc_type varchar2(50) NOT NULL,
-  ssc_board varchar2(100) NOT NULL,
-  ssc_year number(4) NOT NULL,
-  ssc_transcript_path varchar2(15) NOT NULL,
-  ssc_result varchar2(50) NOT NULL
-);
-
-
-CREATE TABLE hsc (
-  app_id varchar2(15) NOT NULL,
-  hsc_roll varchar2(15) NOT NULL,
-  hsc_reg varchar2(15) NOT NULL,
-  hsc_type varchar2(50) NOT NULL,
-  hsc_board varchar2(100) NOT NULL,
-  hsc_year number(4) NOT NULL,
-  hsc_transcript_path varchar2(100),
-  hsc_result varchar2(50) NOT NULL
-);
-
-
-CREATE TABLE undergraduate (
-  app_id varchar2(15) PRIMARY KEY NOT NULL,
-  ug_institution varchar2(15) NOT NULL,
-  ug_type varchar2(50) NOT NULL,
-  ug_cgpa varchar2(100) NOT NULL,
-  ug_pass_year number(4) NOT NULL,
-  ug_transcript_path varchar2(100),
-  ssc_result varchar2(50) NOT NULL
-);
-
-
-CREATE TABLE role (
-  role_id number(10) PRIMARY KEY NOT NULL,
-  role_name varchar2(15) NOT NULL
-);
-
-CREATE TABLE permission (
-  permission_id number(10) PRIMARY KEY NOT NULL,
-  permission_name varchar2(15) NOT NULL
-);
-
-CREATE TABLE role_permission (
-  role_permission_id number(10) PRIMARY KEY NOT NULL,
-  role_id number(10) NOT NULL,
-  permission_id number(10) NOT NULL
+CREATE TABLE Undergrad_Type (
+    ug_type_id NUMBER(10) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY NOT NULL,
+    ug_type_name VARCHAR2(200) NOT NULL
 );
 
 
 
+CREATE TABLE Users (
+    u_id NUMBER(10) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY NOT NULL,
+    u_mail varchar2(200) NOT NULL,
+    u_password varchar2(200) NOT NULL,
+    u_phone varchar2(20),
+    u_name varchar2(200),
+    role_id NUMBER REFERENCES Roles(role_id),
+    u_verified NUMBER(1) DEFAULT 0,
+    u_pic_path varchar2(200),
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-ALTER TABLE application ADD FOREIGN KEY (u_id) REFERENCES user (u_id);
-
-ALTER TABLE ssc ADD FOREIGN KEY (app_id) REFERENCES application (app_id);
-
-ALTER TABLE hsc ADD FOREIGN KEY (app_id) REFERENCES application (app_id);
-
-ALTER TABLE undergraduate ADD FOREIGN KEY (app_id) REFERENCES application (app_id);
-
-ALTER TABLE user ADD FOREIGN KEY (role_id) REFERENCES role (role_id);
-
-ALTER TABLE role_permission ADD FOREIGN KEY (role_id) REFERENCES role (role_id);
-
-ALTER TABLE role_permission ADD FOREIGN KEY (permission_id) REFERENCES permission (permission_id);
-
-INSERT INTO "SYS"."ROLE" (ROLE_ID, ROLE_NAME) VALUES ('1', 'system')
-INSERT INTO "SYS"."ROLE" (ROLE_ID, ROLE_NAME) VALUES ('2', 'admin')
-INSERT INTO "SYS"."ROLE" (ROLE_ID, ROLE_NAME) VALUES ('3', 'student')
+CREATE TABLE Applications (
+    app_id NUMBER(10) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY NOT NULL,
+    u_id NUMBER(10) REFERENCES Users(u_id),
+    a_name varchar2(200),
+    f_name varchar2(200),
+    m_name varchar2(200),
+    a_picpath varchar2(200),
+    a_sigpath varchar2(200),
+    a_dob date,
+    a_phone varchar2(20),
+    a_mail varchar2(200),
+    app_status NUMBER(1) DEFAULT 0,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
-INSERT INTO "SYS"."PERMISSION" (PERMISSION_ID, PERMISSION_NAME) VALUES ('1', 'viewApplication');
-INSERT INTO "SYS"."PERMISSION" (PERMISSION_ID, PERMISSION_NAME) VALUES ('2', 'createApplication');
-INSERT INTO "SYS"."PERMISSION" (PERMISSION_ID, PERMISSION_NAME) VALUES ('3', 'updateApplication');
-INSERT INTO "SYS"."PERMISSION" (PERMISSION_ID, PERMISSION_NAME) VALUES ('4', 'deleteApplication');
+CREATE TABLE Hsc_Ssc (
+    app_id NUMBER(10) PRIMARY KEY REFERENCES Applications(app_id),
+    ssc_roll VARCHAR2(20),
+    ssc_reg VARCHAR2(20),
+    ssc_board VARCHAR2(20),
+    ssc_year NUMBER(10),
+    ssc_transcript_path VARCHAR2(200),
+    ssc_result NUMBER(10, 2),
+    hsc_roll VARCHAR2(20),
+    hsc_reg VARCHAR2(20),
+    hsc_board VARCHAR2(20),
+    hsc_year NUMBER(10),
+    hsc_transcript_path VARCHAR2(200),
+    hsc_result NUMBER(10, 2)
+);
+
+CREATE TABLE Undergrad (
+    app_id NUMBER(10) PRIMARY KEY REFERENCES Applications(app_id),
+    ug_institution  NUMBER(10) REFERENCES Universities(uni_id),
+    ug_subject NUMBER(10) REFERENCES Subjects(sub_id),
+    ug_type VARCHAR2(100),
+    ug_cgpa NUMBER(10, 2),
+    ug_pass_year NUMBER(10),
+    ug_transcript_path VARCHAR2(200)
+);
+
+CREATE TABLE Payment(
+    payment_id NUMBER(10) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY NOT NULL,
+    trx_id VARCHAR2(200),
+    app_id REFERENCES Applications(app_id),
+    amount NUMBER(10, 2),
+    status VARCHAR(50) DEFAULT 'Pending',
+    details VARCHAR(200),
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
-INSERT INTO "SYS"."ROLE_PERMISSION" (ROLE_PERMISSION_ID, ROLE_ID, PERMISSION_ID) VALUES ('1', '3', '2')
-INSERT INTO "SYS"."ROLE_PERMISSION" (ROLE_PERMISSION_ID, ROLE_ID, PERMISSION_ID) VALUES ('2', '2', '1')
-INSERT INTO "SYS"."ROLE_PERMISSION" (ROLE_PERMISSION_ID, ROLE_ID, PERMISSION_ID) VALUES ('3', '2', '3')
-INSERT INTO "SYS"."ROLE_PERMISSION" (ROLE_PERMISSION_ID, ROLE_ID, PERMISSION_ID) VALUES ('4', '2', '4')
+/* Triggres */
 
+CREATE or REPLACE
+TRIGGER SYS.Update_User_Timestamp
+BEFORE INSERT OR UPDATE ON SYS.Users
+FOR EACH ROW
+BEGIN
+    :new.modified_on := SYSTIMESTAMP;
+END;
