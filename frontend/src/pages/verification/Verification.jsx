4205aa557f7f6fdbbf5645123aefc31b227e50fc
Raufun <raufun.nazin13@gmail.com>
2023-02-10 20:22:45 +0600

    no success

--- frontend/src/pages/verification/Verification.jsx
+++ frontend/src/pages/verification/Verification.jsx
@@ -28,6 +28,9 @@ const Verification = () => {
   const toConfirm = () => {
     nav("/confirm");
   };
+  const toHome = () => {
+    nav("/home");
+  };
   return (
     <section className="bg-gray-100 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
@@ -63,11 +66,13 @@ const Verification = () => {
                   OTP expires in{" "}
                   <Countdown daysInHours date={Date.now() + 300000} />
                 </div>
-                <button type="button" className="text-blue-500 hover:underline">Resend code</button>
+                <button type="button" className="text-blue-500 hover:underline">
+                  Resend code
+                </button>
               </div>
               <button
                 type="button"
-                onClick={toConfirm}
+                onClick={toHome}
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Verify email
