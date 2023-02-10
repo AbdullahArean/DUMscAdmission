896690ff2a8e7eb655527f8dd480aab43c101715
Raufun <raufun.nazin13@gmail.com>
2023-02-10 19:58:24 +0600

    otp fix

--- frontend/src/pages/confirmation/Confirmation.jsx
+++ frontend/src/pages/confirmation/Confirmation.jsx
@@ -63,7 +63,7 @@ const Confirmation = () => {
                 Your email could not be verified
               </p>
               <button
-                onClick={toLogin}
+                onClick={toVerify}
                 className="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-blue-900 my-4"
               >
                 Try again
