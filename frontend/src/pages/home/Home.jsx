1d6571996d9e3c194e6bbc7ecb140e5889461f38
Raufun <raufun.nazin13@gmail.com>
2023-02-15 15:40:05 +0600

    notice update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
-import { AiOutlineDownload } from "react-icons/ai";
+import Notice from "../../components/Notice";
+import { RiArrowDownSLine } from "react-icons/ri";
 
 const Home = () => {
   const [isOpen, setOpen] = useState(false);
@@ -62,50 +63,20 @@ const Home = () => {
   return (
     <div className="bg-white dark:bg-gray-900 flex flex-col justify-center">
       <Navbar active={"home"} />
-      <div className="bg-white h-screen dark:bg-gray-900">
-        <div className="w-4/5 text-gray-200 mx-auto mt-14 mb-10 md:mt-32 md:mb-20">
+      <div className="bg-white h-full dark:bg-gray-900">
+        <div className="w-full px-2 md:px-0 md:w-4/5 text-gray-200 md:mx-auto mt-20 mb-10 md:mt-32 md:mb-20">
           <div className="text-xl md:text-3xl text-center dark:text-white mb-5">
             Notice
           </div>
           <div>
             {notice.map((notice, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`flex p-4 mb-4 border-t-4 dark:bg-gray-800 ${notice.type === "error" ? "text-red-800 border-red-300 bg-red-50 dark:text-red-400 dark:border-red-800" : notice.type === "warning" ? "text-yellow-800 border-yellow-300 bg-yellow-50 dark:text-yellow-300 dark:border-yellow-800" : notice.type === "success" ? "text-green-800 border-green-300 bg-green-50 dark:text-green-400 dark:border-green-800" : "border-gray-300 bg-gray-50 dark:bg-gray-800 dark:border-gray-600"}`}
-                  role="alert"
-                >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <div className="ml-3 mr-3 flex justify-between w-full text-sm font-medium">
-                    <div>
-                      {notice.title}: {notice.body}
-                    </div>
-                    <a
-                      href="https://www.africau.edu/images/default/sample.pdf"
-                      className="font-semibold underline hover:no-underline"
-                    >
-                      <AiOutlineDownload className="text-2xl" />
-                    </a>
-                  </div>
-                </div>
-              );
+              return <Notice key={index} notice={notice} />;
             })}
           </div>
         </div>
         {/* FAQ */}
         <div className="w-4/5 mx-auto mt-14 mb-10 md:mt-24 md:mb-20">
-          <div className="text-xl md:text-3xl text-center dark:text-white">
+          <div className="text-xl mb-4 md:text-3xl text-center dark:text-white">
             Frequently Asked Questions
           </div>
           <div>
@@ -124,19 +95,13 @@ const Home = () => {
                   <span>
                     How do I apply for admission through this website?
                   </span>
-                  <svg
-                    data-accordion-icon
-                    className="w-6 h-6 rotate-180 shrink-0"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <RiArrowDownSLine
+                    className={`text-2xl ${
+                      isOpen === true
+                        ? "rotate-180 duration-300 transition-all"
+                        : ""
+                    }`}
+                  />
                 </button>
               </h2>
               <div className={`${isOpen ? "block" : "hidden"}`}>
@@ -159,19 +124,13 @@ const Home = () => {
                   <span>
                     What types of payment are accepted for admission fees?
                   </span>
-                  <svg
-                    data-accordion-icon
-                    className="w-6 h-6 rotate-180 shrink-0"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <RiArrowDownSLine
+                    className={`text-2xl ${
+                      isOpen === true
+                        ? "rotate-180 duration-300 transition-all"
+                        : ""
+                    }`}
+                  />
                 </button>
               </h2>
               <div className={`${isOpen2 ? "block" : "hidden"}`}>
@@ -191,19 +150,13 @@ const Home = () => {
                   <span>
                     How do I know if my application has been accepted?
                   </span>
-                  <svg
-                    data-accordion-icon
-                    className="w-6 h-6 rotate-180 shrink-0"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <RiArrowDownSLine
+                    className={`text-2xl ${
+                      isOpen === true
+                        ? "rotate-180 duration-300 transition-all"
+                        : ""
+                    }`}
+                  />
                 </button>
               </h2>
               <div className={`${isOpen3 ? "block" : "hidden"}`}>
@@ -225,19 +178,13 @@ const Home = () => {
                     Can I make changes to my application after I've submitted
                     it?
                   </span>
-                  <svg
-                    data-accordion-icon
-                    className="w-6 h-6 rotate-180 shrink-0"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <RiArrowDownSLine
+                    className={`text-2xl ${
+                      isOpen === true
+                        ? "rotate-180 duration-300 transition-all"
+                        : ""
+                    }`}
+                  />
                 </button>
               </h2>
               <div className={`${isOpen4 ? "block" : "hidden"}`}>
@@ -257,19 +204,13 @@ const Home = () => {
                   onClick={handleDropDown5}
                 >
                   <span>How do I view notices and FAQs on the website?</span>
-                  <svg
-                    data-accordion-icon
-                    className="w-6 h-6 rotate-180 shrink-0"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+                  <RiArrowDownSLine
+                    className={`text-2xl ${
+                      isOpen === true
+                        ? "rotate-180 duration-300 transition-all"
+                        : ""
+                    }`}
+                  />
                 </button>
               </h2>
               <div className={`${isOpen5 ? "block" : "hidden"}`}>
