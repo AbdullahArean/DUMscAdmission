ba179ac8caa328c71b6b872877e958fa3575e71a
Alve Reduan <alve.reduan.09@gmail.com>
2023-02-15 16:13:44 +0600

    Submission Page SSLCOMMERZ

--- frontend/src/pages/submission/Submission.jsx
+++ frontend/src/pages/submission/Submission.jsx
@@ -47,12 +47,17 @@ const Submission = () => {
               </th>
               <td class="px-6 py-4">15-FEB-2023</td>
               <td class="px-6 py-4">
-                <button
-                  type="button"
-                  class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                <a
+                  target="_blank"
+                  href="https://sslcommerz.netlify.app/checkout"
                 >
-                  Pay Now
-                </button>
+                  <button
+                    type="button"
+                    class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                  >
+                    Pay Now
+                  </button>
+                </a>
               </td>
             </tr>
           </tbody>
