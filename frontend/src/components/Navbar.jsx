3e0714b9630b5c2a84b62e5c8536ecf9b6d4506e
Alve Reduan <alve.reduan.09@gmail.com>
2023-02-15 16:18:19 +0600

    Submission Page Routing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -107,6 +107,9 @@ const Navbar = ({ active }) => {
             </li> */}
             <li>
               <div
+                onClick={() => {
+                  nav("/submission");
+                }}
                 className={
                   active === "submission"
                     ? "block cursor-pointer py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
