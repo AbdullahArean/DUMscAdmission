86993759e8fbe6e406ff279fad9ac4a8acd19d28
Raufun <raufun.nazin13@gmail.com>
2023-02-27 14:01:08 +0600

    updates

--- frontend/src/pages/Submission.jsx
+++ frontend/src/pages/Submission.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
