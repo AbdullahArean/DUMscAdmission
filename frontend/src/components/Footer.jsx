5dc1734ba326cdbc06f3b7adae0733626a993636
Raufun <raufun.nazin13@gmail.com>
2023-03-21 11:27:35 +0600

    footer align

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-100 dark:bg-gray-800 text-black dark:text-white text-center md:font-semibold font-xs md:font-xl py-2 md:py-6">
+    <div className="bg-gray-100 bottom-0 left-0 dark:bg-gray-800 text-black dark:text-white text-center md:font-semibold font-xs md:font-xl py-2 md:py-6">
       © 2023 Department of Computer Science and Engineering, University of
       Dhaka, Dhaka, Bangladesh
     </div>
