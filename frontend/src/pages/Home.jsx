ef9a8c295689e7e6f057608697fbf2d4a2539b9c
reduan2660 <alve.reduan.09@gmail.com>
2023-05-21 21:49:16 +0600

    Webinar & Updated Timeline

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,6 +11,8 @@ import api from "../api";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import header from "../resources/header.png";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Modal } from "antd";
 
 const Home = () => {
   const { Panel } = Collapse;
@@ -18,6 +20,7 @@ const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useGlobalState("isLoggedIn");
   const [jwt, setJwt] = useGlobalState("jwt");
   const [notices, setNotices] = useState([]);
+  const [webinarModalOpen , setWebinarModalOpen] = useState(false);
 
   const getNotice = () => {
     api
@@ -38,6 +41,12 @@ const Home = () => {
 
   const notice = [
     {
+      title: "Updated Timeline for CSEDU MSc Admission",
+      body: "This documents states the extended & updated timeline for CSEDU MSc Admission.",
+      link: "https://drive.google.com/file/d/1ELlcS_0JOCEWBB28NiSQ1ql0UdwNUXSt/view?usp=sharing",
+      type: "warning"
+    },
+    {
       title: "Brochure for CSEDU MSc Admission",
       body: `This document outlines the graphical representation of eligibility criteria and admission procedure for the MS in Computer Science and Engineering degree at the University of Dhaka. The application period for the upcoming semester runs from March 31 to May 15, with the admission test taking place on June 3.`,
       link: "https://drive.google.com/file/d/1H6IFFaMp6hx4Plq0Rrt5Iz6vVlDblTTu/view?usp=sharing",
@@ -55,14 +64,51 @@ const Home = () => {
   ];
 
   useEffect(() => {
-    getNotice();
+    // getNotice();
   }, []);
 
   return (
     <div className="bg-white relative dark:bg-gray-900 flex flex-col justify-center text-gray-800 dark:text-white">
-      <div className="w-full  font-mono  font-semibold underline italic text-xl md:text-4xl text-center md:w-3/5 md:mx-auto mt-24 md:mt-28">
-        The application process is still ongoing.
+      
+      {/* Webinar */}
+      <div
+      className={`cursor-pointer mt-24 flex p-4 mb-4 border-t-4 dark:bg-gray-800 text-green-800 border-green-300 bg-green-50 dark:text-green-400 dark:border-green-800`}
+      role="alert"
+      onClick={() => setWebinarModalOpen(true)}
+    >
+      <svg
+        className="flex-shrink-0 w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+      <div className="ml-3 mr-3 flex justify-between items-center w-full text-sm font-medium">
+        <div className="w-full mr-2 md:mr-8">
+          <div className="flex justify-between">
+            <div className="text-md md:text-lg">Webinar on 25th May 9.00 PM with Professor Md. Mamun Rashid</div>
+            {/* <div>{notice.created_by}</div> */}
+            {/* <div>{notice.created_on}</div> */}
+          </div>
+          
+        </div>
+        <a
+          target="_blank"
+          href={notice.link}
+          className="font-semibold flex flex-col-reverse md:flex-row justify-center md:gap-x-2 items-center hover:underline no-underline"
+        >
+          <p className="text-xs md:text-md cursor-pointer">View Details</p>
+        </a>
       </div>
+    </div>
+
+      {/* Hero Image */}
+
       <img
         src={header}
         className="mt-16 md:mt-8 md:w-1/2 mx-auto"
@@ -203,18 +249,18 @@ const Home = () => {
                 <b>Application Starts:</b> March 31, 2023
                 <br />
                 <b>Application Ends: </b>
-                May 15, 2023
+                June 5, 2023
                 <br />
                 <br />
                 <b>Other important dates:</b>
                 <br />
-                <b>Admit Card Publish:</b> From May 21, 2023
+                <b>Admit Card Publish:</b> Within June 8, 2023
                 <br />
-                <b>Admission Test:</b> June 3, 2023
+                <b>Admission Test:</b> June 9, 2023, Friday, 11:00 AM.
                 <br />
-                <b>Admission Test Result:</b> June 6, 2023
+                <b>Admission Test Result:</b> June 10, 2023
                 <br />
-                <b>Communication Skill Test:</b> June 10, 2023
+                <b>Communication Skill Test & Viva:</b> June 15, 2023, Thursday, 2:00 PM
                 <br />
                 <b>Final Result:</b> June 17, 2023
                 <br />
@@ -222,7 +268,7 @@ const Home = () => {
                 <br />
                 <b>Admission End:</b> June 21, 2023
                 <br />
-                <b>Class Start:</b> 1st week of July, 2023
+                <b>Class Start:</b> 2nd week of July, 2023
                 <br />
               </p>
             </Panel>
@@ -248,6 +294,49 @@ const Home = () => {
         </div>
       </div>
       <Footer />
+
+      <Modal
+          title="Invitation to webinar"
+          centered
+          open={webinarModalOpen}
+          onOk={() => setWebinarModalOpen(false)}
+          onCancel={() => setWebinarModalOpen(false)}
+          okText="Close"
+          cancelButtonProps={{ style: { display: "none" } }}
+        >
+
+          <div>
+            <p>
+            Dear Students,  <br/>
+              We (CSE, DU) are going to arrange a webinar on 25th May 9.00 PM in the following link. 
+              Professor Md. Mamun Rashid is inviting you to a scheduled BdREN Zoom meeting.
+              <br/>
+              <br/>
+              Topic: Webinar on MS Admission 2023  <br/>
+              Time: May 25, 2023 09:00 PM Astana, Dhaka  <br/>  <br/>
+
+              -- Join BdREN Zoom Meeting from Laptop or Mobile:  <br/>
+              <a className="underline text-blue-300" href="https://bdren.zoom.us/j/69588484234?pwd=a1dWTG9TU0RKSy9BMmVoamtFWGg0QT09 "> https://bdren.zoom.us/j/69588484234?pwd=a1dWTG9TU0RKSy9BMmVoamtFWGg0QT09 </a>  <br/><br/>
+
+              Meeting ID: 695 8848 4234   <br/>
+              Password: 529036  <br/>
+
+              The webinar will discuss the following aspects of the MS program in CSE, DU and finally answer the admission related questions.:  <br/>
+ 
+              1. Admission Test Pattern  <br/>
+              2. Admission Fees and Other Educational Expenses  <br/>
+              3. Contemporary Opportunities in CSE  <br/>
+              4. Why CSEDU should be your choice  <br/>
+              5. Adaptability with the mainstream students  <br/>
+              6. Scholarship Opportunities  <br/>  <br/>
+
+              Apart from the above agenda students may discuss any other academic and admission related issues. Advanced questions and issues on the academic program and admission test are welcome in the following link.
+            </p>
+          </div>
+        </Modal>
+
+
+
     </div>
   );
 };
