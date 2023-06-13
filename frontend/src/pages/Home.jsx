79fbcc7f82d8c143bc3e6f54844826676b7c95d8
reduan2660 <alve.reduan.09@gmail.com>
2023-06-13 13:47:38 +0600

    Viva Notice

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -78,7 +78,7 @@ const Home = () => {
         className={`mt-24 cursor-pointer flex p-4 mb-4 border-t-4 dark:bg-gray-800 text-yellow-800 border-yellow-300 bg-yellow-50 dark:text-yellow-300 dark:border-yellow-800`}
         role="alert"
         onClick={() => {
-          nav("/submission");
+          window.open("https://drive.google.com/file/d/1fPQ9JORZqLsQHghkbqgD_UP1h_r85iHs/view?usp=share_link", "_blank");
         }}
       >
         <svg
@@ -97,61 +97,16 @@ const Home = () => {
           <div className="w-full mr-2 md:mr-8">
             <div className="flex justify-center">
               <div className="text-md md:text-lg animate__animated animate__flash animate__repeat-3">
-                Result has been published
+                Viva Notice for selected applicants on 15th June 1:30 PM
               </div>
               {/* <div>{notice.created_by}</div> */}
               {/* <div>{notice.created_on}</div> */}
             </div>
           </div>
 
-          <p className="text-xs md:text-md cursor-pointer">Get Result</p>
+          <p className="text-xs md:text-md cursor-pointer">Download</p>
         </div>
       </div>
-
-      {/* Webinar */}
-      {isLoggedIn ? (
-        <>
-          <div
-            className={`cursor-pointer flex p-4 mb-4 border-t-4 dark:bg-gray-800 text-green-800 border-green-300 bg-green-50 dark:text-green-400 dark:border-green-800`}
-            role="alert"
-          >
-            <svg
-              className="flex-shrink-0 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <div className="ml-3 mr-3 flex justify-between items-center w-full text-sm font-medium">
-              <div className="w-full mr-2 md:mr-8">
-                <div className="flex justify-center">
-                  <div className="text-md md:text-lg">
-                    Previous Questions of CSEDU MSc Admission Program
-                  </div>
-                  {/* <div>{notice.created_by}</div> */}
-                  {/* <div>{notice.created_on}</div> */}
-                </div>
-              </div>
-              <a
-                target="_blank"
-                href="https://drive.google.com/file/d/1wFr9UBjuaryqSlST7wxrdtr1R6qP7j6u/view?usp=share_link"
-                className="font-semibold flex flex-col-reverse md:flex-row justify-center md:gap-x-2 items-center hover:underline no-underline"
-              >
-                <p className="text-xs md:text-md cursor-pointer">
-                  View Details
-                </p>
-              </a>
-            </div>
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
 
       {/* Hero Image */}
 
